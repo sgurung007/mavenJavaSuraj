ed1e5dfda0acf63cc1ac378372b1365ceb4adaed
surajGurung <sgrg007@gmail.com>
2020-07-07 21:42:31 -0500

    drag and drop for slider and for resize boxes

--- src/main/java/org/example/seleniumWebdriver/Test1.java
+++ src/main/java/org/example/seleniumWebdriver/Test1.java
@@ -2,7 +2,9 @@ package org.example.seleniumWebdriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,11 +19,17 @@ public class Test1 {
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.homedepot.com/");
-        boolean elementPresent = isElementPresent(By.xpath("//a[@class='MainFlyout__link'][contains(text(),'Blinds & Window Treatment')]"));
-        System.out.println(elementPresent);
-        boolean project_calculatorsIs = isElementPresent(By.linkText("Project Calculators"));
-        System.out.println(project_calculatorsIs);
+//        driver.get("http://www.way2automation.com/");
+////        WebElement selenium = driver.findElementByXPath("//body/div[@class='layout-theme animated-css']/div[@id='wrapper']/header[@class='header']/div[@class='nav_base']/div[@class='container']/nav[@class='navbar yamm']/div[@id='navbar-collapse-1']/ul[@class='nav navbar-nav']/li[3]");
+////        Actions actions = new Actions(driver);
+////        actions.moveToElement(selenium).perform();
+////        driver.findElementByXPath("//a[contains(text(),'Selenium JAVA Classroom Training')]").click();
+        driver.get("https://www.flipkart.com/clothing-and-accessories/pr?sid=clo&otracker=categorytree&p%5B%5D=facets.ideal_for%255B%255D%3DMen&otracker=nmenu_sub_Men_0_Clothing&p%5B%5D=facets.ideal_for%255B%255D%3Dmen&p%5B%5D=facets.price_range.from%3DMin&p%5B%5D=facets.price_range.to%3D1000");
+        WebElement sliderWebElement = driver.findElementByXPath("//div[@class='gl5Kwg']");
+        int sliderOffx = sliderWebElement.getSize().getWidth();
+        System.out.println(sliderOffx);
+        new Actions(driver).dragAndDropBy(sliderWebElement,sliderOffx,0).perform();
+
         driver.quit();
     }
 //utility method to see if Webelement is present
