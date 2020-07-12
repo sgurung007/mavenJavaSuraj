252ef7035e892d6f0f2163fb33dcb8219dc1bdef
surajGurung <sgrg007@gmail.com>
2020-07-11 22:28:42 -0500

    testng added

---
+++ src/main/java/org/example/testng/TestCaseOne.java
@@ -0,0 +1,16 @@
+package org.example.testng;
+
+import org.testng.annotations.Test;
+
+public class TestCaseOne {
+    @Test
+    public void doLogin() {
+        System.out.println("Prinitng from doLogin");
+    }
+
+    @Test
+    public void doUserReg() {
+        System.out.println("prinitng form user registration");
+    }
+
+}
